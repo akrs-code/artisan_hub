a83463d91fea741cf0e54475fcb3d6fd7453b658
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-06-12 13:14:11 +0800

    chore(deps): add frontend dependencies, config files, and utility helpers

--- backend/modules/users/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers } = require('./user.controller');
-
-// Maps to /api/users/
-router.route('/')
-  .get(getUsers);
-
-module.exports = router;
