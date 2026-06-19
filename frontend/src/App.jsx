f6d9cd7903488b0d5042939e1c2de9210c94416c
mr-mangoranda <habibmangoranda15@gmail.com>
2026-06-20 01:53:01 +0800

    refractor(admin): transferred all files to admin branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RootLayout from './layouts/RootLayout';
 import { BuyerSidebar, SellerSidebar, AdminSidebar } from './components/RoleBasedSidebar';
 import WorkspaceView from './components/WorkspaceView';
+import Overview from './pages/admin/overview';
+import Verify from './pages/admin/verify';
+import Moderate from './pages/admin/moderate';
+import Users from './pages/admin/users';
+import Logs from './pages/admin/logs';
+import Disputes from './pages/admin/disputes';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/admin/*" element={<RootLayout sidebarContent={<AdminSidebar />} />}>
+      <Route path="/admin/*" element={<RootLayout sidebarContent={<AdminSidebar />} />} >
+        <Route path="" element={<Overview />} />
+        <Route path="verify" element={<Verify />} />
+        <Route path="moderate" element={<Moderate />} />
+        <Route path="users" element={<Users />} />
+        <Route path="logs" element={<Logs />} />
+        <Route path="disputes" element={<Disputes />} />
         <Route path="*" element={<WorkspaceView />} />
       </Route>
 
